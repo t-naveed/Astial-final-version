059bd57f87bbba1ba49e493f54077fffe8caae0e
Tasin Naveed <90913621+tan7-njit@users.noreply.github.com>
2022-10-02 14:16:44 -0400

    Update login.py

--- apps/login.py
+++ apps/login.py
@@ -1,9 +1,21 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import database as db
+import os
+from twilio.rest import Client
+from dotenv import load_dotenv
 
+def send_message():
+    account_sid = st.secrets["account_sid"]
+    auth_token = st.secrets["auth_token"]
+    client = Client(account_sid, auth_token)
 
-
+    message = client.messages.create(
+        body="This is from astia",
+        from_=st.secrets["twilio_number"],
+        to='+18622357825'
+    )
+    st.write(message.body)
 def app():
     # --- USER AUTHENTICATION ---
     users = db.fetch_all_users()
@@ -23,7 +35,16 @@ def app():
         st.warning("Please enter your username and password or SignUp")
 
     if authentication_status:
-        st.title(f"Welcome {name}")
-        st.subheader("We need to set our smishing script here. User will upload employees phone number and click a button to launch attack.")
+        st.subheader(f"Welcome {name}. You can select each tab below for upload data and launching smishing")
+        tab1, tab2= st.tabs(["Upload Data", "Launch Attack"])
+        with tab1:
+            uploaded_file = st.file_uploader("Select a file contains employees name and phone number")
+
+
+        with tab2:
+            if st.button("Launch Smishing"):
+                send_message()
+
+
         authenticator.logout("Logout", "main")
 
