8dc558bb695d6193707a1ce5618b02253cadaee0
Tasin Naveed <90913621+tan7-njit@users.noreply.github.com>
2022-10-02 21:11:18 -0400

    Update signup.py

--- apps/signup.py
+++ apps/signup.py
@@ -5,29 +5,33 @@ import database as db
 
 def app():
     st.write("##")
-    st.subheader("Create an Account")
-    names = []
-    usernames = []
-    passwords = []
-    full_name = st.text_input('Full name')
-    names.append(full_name)
-    usr_name = st.text_input('Username')
-    usernames.append(usr_name)
-    set_passwd = st.text_input('Password', type='password')
-    passwords.append(set_passwd)
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.subheader("Create an Account")
+        names = []
+        usernames = []
+        passwords = []
+        full_name = st.text_input('Full name')
+        names.append(full_name)
+        usr_name = st.text_input('Username')
+        usernames.append(usr_name)
+        set_passwd = st.text_input('Password', type='password')
+        passwords.append(set_passwd)
 
-    hashed_passwords = stauth.Hasher(passwords).generate()
-    users = db.fetch_all_users()
-    usernames_from_db = [user["key"] for user in users]
-    if names[0] == "" or usernames[0] == "" or passwords[0] == "":
-        st.warning("Must fill all the fields")
-    for i in range(len(usernames_from_db)):
-        if (usernames_from_db[i]) == (usernames[0]):
-            st.error("Username already exist")
+        hashed_passwords = stauth.Hasher(passwords).generate()
+        users = db.fetch_all_users()
+        usernames_from_db = [user["key"] for user in users]
+        if names[0] == "" or usernames[0] == "" or passwords[0] == "":
+            st.warning("Must fill all the fields")
+        for i in range(len(usernames_from_db)):
+            if (usernames_from_db[i]) == (usernames[0]):
+                st.error("Username already exist")
 
-    st.write("##")
-    if st.button('SignUp'):
-        for (username, name, hashed_passwords) in zip(usernames, names, hashed_passwords):
-            db.insert_user(username, name, hashed_passwords)
-            st.success("You have successfully created an account.Go to the Login Menu to login")
+        st.write("##")
+        if st.button('SignUp'):
+            for (username, name, hashed_passwords) in zip(usernames, names, hashed_passwords):
+                db.insert_user(username, name, hashed_passwords)
+                st.success("You have successfully created an account.Go to the Login Menu to login")
+    with right_column:
+        st.write("")
 
