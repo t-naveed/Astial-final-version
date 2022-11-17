0e79fd41d111e28fde353fd3d9e88668450b35f2
Tasin Naveed <90913621+tan7-njit@users.noreply.github.com>
2022-11-17 12:55:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,144 +1,145 @@
+from apps.login import *
 import requests
-import streamlit as st
-
-st.set_page_config(page_title="Threat Map", page_icon=":fishing_pole_and_fish:", layout="wide")
-
-
-def load_threats(page_number):
-    key = st.secrets["key"]
-    headers = {'X-OTX-API-KEY': key}
-    # params = {'page': '5',}
-    response = requests.get(f'https://otx.alienvault.com/api/v1/pulses/subscribed?page={page_number}', headers=headers)
-    data = response.json()
-    list = data['results']
-    threat_collector(list)
+from streamlit_option_menu import option_menu
+from streamlit_lottie import st_lottie
+from multiapp import MultiApp
+from apps import login, signup
 
 
-def indicator_details(list_of_indicator):
-    indicator_dict = {'indicator': '', 'type': '', 'created': ''}
-    st.warning("Indicator of compromise", icon="⚠️")
-    indicator_list = []
-    indicator_type_list = []
-    date_time_list = []
-    table = '''
-            <table style="width:100%;table-layout: fixed;">
-                <tr>
-                    <th style="width:50%;border:1px solid black;border-collapse: collapse;">Indicator</th>
-                    <th style="width:25%;border:1px solid black;border-collapse: collapse;">Type</th>
-                    <th style="width:25%;border:1px solid black;border-collapse: collapse;">Date of Creation</th>
-                    </tr>
-            </table>
-            '''
-    st.markdown(table, unsafe_allow_html=True)
-    for i in range(len(list_of_indicator)):
-        for key, val in list_of_indicator[i].items():
-            indicator_dict[key] = val
-        indicator_list.append(indicator_dict["indicator"])
-        indicator_type_list.append(indicator_dict["type"])
-        date_time_list.append(indicator_dict["created"])
-        updated_table = f'''
-        <table style="width:100%;table-layout: fixed;">
-        <tr>
-            <td style="width:50%;overflow-y: hidden;">{indicator_list[i]}</td>
-            <td style="width:25%">{indicator_type_list[i]}</td>
-            <td style="width:25%">{date_time_list[i]}</td>
-        </tr>
-        </table>
-        '''
-        st.markdown(updated_table, unsafe_allow_html=True)
+st.set_page_config(page_title="Astia a Smishing Platform", page_icon=":fishing_pole_and_fish:", layout="wide")
 
 
-def threat_collector(list_of_json):
-    final_dict = {"name": "", "description": "", "adversary": "", "indicators": ""}
-    for i in range(len(list_of_json)):
-        for key, val in list_of_json[i].items():
-            final_dict[key] = val
-        st.info('Name: ' + final_dict["name"])
-        st.write('Description: ' + final_dict["description"])
-        st.write('Adversary: ' + final_dict["adversary"])
-        with st.expander("Show IOC Details"):
-            (indicator_details(final_dict["indicators"]))
-        st.write('**********************************************************************************'
-                 '************************************************************************************')
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 
-def post_url_sample(url):
-    key = st.secrets["key"]
-    headers = {'X-OTX-API-KEY': key}
-    api = 'https://otx.alienvault.com/api/v1/indicators/submit_url'
-    myobj = {'url': f'{url}'}
-    result = requests.post(api, json=myobj, headers=headers)
-    if result.status_code == 200:
-        st.success("url Sample submitted succesfully")
-
-def get_last_result():
-    key = st.secrets["key"]
-    headers = {'X-OTX-API-KEY': key}
-    result = requests.get('https://otx.alienvault.com/api/v1/indicators/submitted_urls', headers=headers)
-    data = result.json()
-    list = data['results']
-    result_dict = {'url': '', 'add_date': '', 'sent_date': '', 'complete_date': '', 'tlp': ''}
-    for i in range(1):
-        for key, val in list[i].items():
-            result_dict[key] = val
-    st.write(f'Submitted url: {result_dict["url"]}  \nAdded Date: {result_dict["add_date"]}  \nSent Date: {result_dict["sent_date"]}  \nCompleted Date: {result_dict["complete_date"]}  \nTLP: {result_dict["tlp"]}')
-
-def get_all_result():
-    key = st.secrets["key"]
-    headers = {'X-OTX-API-KEY': key}
-    result = requests.get('https://otx.alienvault.com/api/v1/indicators/submitted_urls', headers=headers)
-    data = result.json()
-    list = data['results']
-    result_dict = {'url': '', 'add_date': '', 'sent_date': '', 'complete_date': '', 'tlp': ''}
-    url_list = []
-    add_date = []
-    sent_date = []
-    complete_date = []
-    tlp_list = []
-    for i in range(len(list)):
-        for key, val in list[i].items():
-            result_dict[key] = val
-
-        url_list.append(result_dict['url'])
-        add_date.append(result_dict['add_date'])
-        sent_date.append(result_dict['sent_date'])
-        complete_date.append(result_dict['complete_date'])
-        tlp_list.append(result_dict['tlp'])
-        st.write(
-            f'Submitted url: {url_list[i]}  \nAdded Date: {add_date[i]}  \nSent Date: {sent_date[i]}  \nCompleted Date: {complete_date[i]}  \nTLP: {tlp_list[i]}')
+local_css("style/style.css")
 
 
-with st.container():
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.subheader("Top Threats")
-        option = st.selectbox(
-            'Load more threats by selecting next pages',
-            ('1', '2', '3', '4', '5'))
-        if option == '1':
-            load_threats(1)
-        if option == '2':
-            load_threats(2)
-        if option == '3':
-            load_threats(3)
-        if option == '4':
-            load_threats(4)
-        if option == '5':
-            load_threats(5)
-        st.write('You are viewing page:', option)
-    with right_column:
-        st.subheader("Analyse Malicious url")
-        url = st.text_input('Paste the suspicious url here and hit enter', '')
-        if url != '':
-            post_url_sample(url)
-            with st.expander("Check this result:"):
-                get_last_result()
-            with st.expander("Check all results submitted"):
-                get_all_result()
+# -------- Lottie Asset -------- #
+def load_lottie(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-        st.subheader("Analyse Malicious File")
-        uploaded_file = st.file_uploader("Upload Suspicious File to Analyze")
+
+animation = load_lottie("https://assets4.lottiefiles.com/packages/lf20_mcvtkrvc.json")
+animation4 = load_lottie("https://assets5.lottiefiles.com/packages/lf20_isbiybfh.json")
+animation5 = load_lottie("https://assets4.lottiefiles.com/packages/lf20_joexwr4o.json")
+service3 = load_lottie("https://assets7.lottiefiles.com/packages/lf20_zdtukd5q.json")
 
 
 
 
+# ----------- Sidebar ----------- #
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Astia Dashboard",
+        options=["Home", "Services", "Pricing", "Support"],
+        icons=["house", "card-checklist", "tags", "headset"],
+        menu_icon=[""],
+        default_index=0,
+        styles={
+            "nav-link": {
+                "font-size": "20px",
+                "--hover-color": "rgb(3, 171, 112)"
+            },
+            "nav-link-selected": {"background-color": "rgb(3, 171, 112)", "color": "white"}
+        }
+    )
+
+# ------------- Home Page ------------------ #
+if selected == "Home":
+    string = "<h3 style = 'color:white; background-color:rgb(3, 171, 112); margin-top:-15px; font-style:italic;width: 100%;'>Astia<sub><i>Smishing Analysis Platform</i></sub></h3>"
+    st.write(string, unsafe_allow_html=True)
+    with st.sidebar:
+        st.warning("Login or Signup by clicking Services for Launching Smishing Campaign")
+    with st.container():
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write("###")
+            st.write("###")
+            st.subheader("This is where Cybersecurity meets Data Science")
+            st.write("###")
+            string = "Astia is a cybersecurity startup, specifically within user training awareness. The goal is to " \
+                     "design a smishing platform that is capable of launching smishing campaigns and then measure " \
+                     "analytics and metrics that has been generated by a campaign to make it easy to see patterns to " \
+                     "mitigate real-world cyber attacks. "
+            st.write(string, unsafe_allow_html=True)
+
+        with right_column:
+            st.write("###")
+            st.write("###")
+            st_lottie(animation, height=300, key="hacking")
+    st.write("###")
+    with st.container():
+        st.subheader("Data Analytics & Visualization")
+        st.write("###")
+        string = "It's important to not only show that we are capable of launching campaigns but we want to also offer a" \
+                 "myriad of data generated by a campaign to help security teams take a data-driven approach to locking down their company."
+        st.write(string, unsafe_allow_html=True)
+
+# ------------- Service Page ------------------ #
+if selected == "Services":
+
+    with st.container():
+        string = "<h3 style = 'color:white; background-color:rgb(3, 171, 112); margin-top:-15px; font-style:italic;width: 100%;'>Astia<sub><i>Smishing Analysis Platform</i></sub></h3>"
+        st.write(string, unsafe_allow_html=True)
+        st.write("##")
+
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.subheader("Smishing Campaign")
+            st.write("While your employees are your greatest asset, they can also be your biggest threat regarding "
+                     "cyber security 🚨Between phishing, social engineering attacks, malware distribution, "
+                     "password theft and breaches, there has never been a greater need for non-technical employees to "
+                     "maintain a cyber culture.")
+
+        with right_column:
+            st_lottie(service3, height=200, key="service3")
+        with st.sidebar:
+            app = MultiApp()
+            app.add_app("Login", login.app)
+            app.add_app("SignUp", signup.app)
+            app.run()
+        login.success()
+
+# ------------- Support Page ------------------ #
+if selected == "Support":
+    string = "<h3 style = 'color:white; background-color:rgb(3, 171, 112); margin-top:-15px; font-style:italic;width: 100%;'>Astia<sub><i>Smishing Analysis Platform</i></sub></h3>"
+    st.write(string, unsafe_allow_html=True)
+    st.write("##")
+    st.write("##")
+    with st.container():
+        st.subheader("Need Help? Astia is one step away from you!")
+        st.write("##")
+        contact_form = """
+        <form action="https://formsubmit.co/naveedanwar808@gmail.com" method="POST">
+        <input type="hidden" value="false" name="_captcha">
+     <input type="text" placeholder="Your name" name="name" required>
+     <input type="email" placeholder="Your email" name="email" required>
+     <textarea name="message" placeholder="Your message" required></textarea>
+     <button type="submit">Send</button>
+    </form>
+        """
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.markdown(contact_form, unsafe_allow_html=True)
+        with right_column:
+            st_lottie(animation4, height=300, key="service")
+
+# ------------- Pricing Page ------------------ #
+if selected == "Pricing":
+    string = "<h3 style = 'color:white; background-color:rgb(3, 171, 112); margin-top:-15px; font-style:italic;width: 100%;'>Astia<sub><i>Smishing Analysis Platform</i></sub></h3>"
+    st.write(string, unsafe_allow_html=True)
+    with st.container():
+        st.write("##")
+        st.write("##")
+        left_column, right_column = st.columns(2)
+        with left_column:
+            st.write("")
+        with right_column:
+            st.write("")
